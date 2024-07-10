9fe0df4e3a0eff5885930795e17a6eb9e2f327a3
armando hysaj <armandohysaj7@gmail.com>
2024-07-10 21:44:58 +0200

    change directory name
    project name

--- src/app/Cryptolytics/page.tsx
+++ src/app/Cryptolytics/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -7,13 +7,18 @@ import Portfolio from "../components/portfolio/portfolio";
 import { useState } from "react";
 
 export default function CryptolyticsPage() {
-    const [selectedCoin, setSelectedCoin] = useState<any>(null);
-    const [selectedCoinLoaded, setSelectedCoinLoaded] = useState(false);
-    return (
-        <>
-        <Header setSelectedCoin={setSelectedCoin}  setSelectedCoinLoaded={setSelectedCoinLoaded} />
-        <Portfolio selectedCoin={selectedCoin} selectedCoinLoaded={selectedCoinLoaded} />
-        </>
-    );
-  }
-
+  const [selectedCoin, setSelectedCoin] = useState<any>(null);
+  const [selectedCoinLoaded, setSelectedCoinLoaded] = useState(false);
+  return (
+    <>
+      <Header
+        setSelectedCoin={setSelectedCoin}
+        setSelectedCoinLoaded={setSelectedCoinLoaded}
+      />
+      <Portfolio
+        selectedCoin={selectedCoin}
+        selectedCoinLoaded={selectedCoinLoaded}
+      />
+    </>
+  );
+}
