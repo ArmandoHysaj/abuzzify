fecd49d60ec2733512951c321576efc781d9f6a2
ArmandoHysaj <96386739+ArmandoHysaj@users.noreply.github.com>
2024-08-23 21:28:47 +0200

    adjustments

--- src/app/components/CoinCarouselBar/CoinCarouselBar.tsx
+++ src/app/components/CoinCarouselBar/CoinCarouselBar.tsx
@@ -2,17 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./coin-carousel-bar.scss";
 
-
 const CoinCarouselBar = () => {
   const [coins, setCoins] = useState<any[]>([]);
 
   useEffect(() => {
     fetchSimilarCoins();
   }, []);
-
-  useEffect(() => {
-    console.log(coins);
-  }, [coins]);
 
   const fetchSimilarCoins = async () => {
     try {
@@ -25,19 +20,40 @@ const CoinCarouselBar = () => {
 
   return (
     <div className="coin-carousel-bar">
-      {coins.length > 0 ? (
-        coins.map((coin) => (
-          <div key={coin.id} className="coin-bar">
-            <span className="coin-name">{coin.name}</span>
-            <span className="coin-price">${coin.price_usd}</span>
-            <span className={`coin-change ${
-                        coin.percent_change_24h > 0 ? "green" : "red"
-                      }`}>{coin.percent_change_24h}%</span>
-          </div>
-        ))
-      ) : (
-        <div className="title red">No coins available</div>
-      )}
+      <div className="coin-carousel-track">
+        {coins.length > 0 ? (
+          <>
+            {coins.map((coin) => (
+              <div key={coin.id} className="coin-bar">
+                <span className="coin-name">{coin.name}</span>
+                <span className="coin-price">${coin.price_usd}</span>
+                <span
+                  className={`coin-change ${
+                    coin.percent_change_24h > 0 ? "green" : "red"
+                  }`}
+                >
+                  {coin.percent_change_24h}%
+                </span>
+              </div>
+            ))}
+            {coins.map((coin) => (
+              <div key={`dup-${coin.id}`} className="coin-bar">
+                <span className="coin-name">{coin.name}</span>
+                <span className="coin-price">${coin.price_usd}</span>
+                <span
+                  className={`coin-change ${
+                    coin.percent_change_24h > 0 ? "green" : "red"
+                  }`}
+                >
+                  {coin.percent_change_24h}%
+                </span>
+              </div>
+            ))}
+          </>
+        ) : (
+          <div className="loading"></div>
+        )}
+      </div>
     </div>
   );
 };
